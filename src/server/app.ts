c4ceaa612d8d17b5db93a718f98f2764767f2f6b
Alfonso <alfonso.vitores@gmail.com>
2024-10-02 01:30:11 +0200

    Cors instalation to allow calls from loalhost

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 
 import taskRoutes from '../routes/taskRoutes'
 
@@ -7,7 +8,9 @@ const app = express();
 //middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
+app.use(cors({
+  origin: 'http://localhost:3001'
+}));
 
 //routes
 app.get("/welcome", (req, res) => {
